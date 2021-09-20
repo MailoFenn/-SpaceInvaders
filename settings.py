7886541f8de804d6e89d60b40f881aefd6af41d9
MailoFenn <nikitakulkov7@gmail.com>
2021-09-20 22:14:00 +0300

    add move ship

--- settings.py
+++ settings.py
@@ -1,5 +1,7 @@
 class Settings:
     def __init__(self):
-        self.screen_width = 1200
-        self.screen_height = 800
+        self.screen_width = 1920
+        self.screen_height = 900
         self.bg_color = (50, 50, 50)
+
+        self.ship_speed = 1.9
--- ship.py
+++ ship.py
@@ -5,10 +5,23 @@ class Ship:
     def __init__(self, ai_games):
         self.screen = ai_games.screen
         self.screen_rect = ai_games.screen.get_rect()
+        self.settings = ai_games.settings
 
         self.image = pygame.image.load('img/ship.png')
         self.rect = self.image.get_rect()
         self.rect.midbottom = self.screen_rect.midbottom
+        self.x = float(self.rect.x)
+
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        if self.moving_right and self.x < self.screen_rect.right - 100:
+            self.x += self.settings.ship_speed
+        elif self.moving_left and self.x > 0:
+            self.x -= self.settings.ship_speed
+
+        self.rect.x = self.x
 
     def blitme(self):
         self.screen.blit(self.image, self.rect)
