d24c4721e667e9265f8e50fc1057da9ff05c4b95
Kulikov Nikita <nikitakulkov7@gmail.com>
2021-09-30 11:58:29 +0300

    add fire

--- settings.py
+++ settings.py
@@ -1,7 +1,12 @@
 class Settings:
     def __init__(self):
-        self.screen_width = 1920
+        self.screen_width = 1080
         self.screen_height = 900
         self.bg_color = (50, 50, 50)
 
         self.ship_speed = 1.9
+
+        self.bullet_speed = 1
+        self.bullet_width = 3
+        self.bullet_height = 10
+        self.bullet_color = (255, 60, 60)
