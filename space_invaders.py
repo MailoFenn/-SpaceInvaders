55d2ef7cd7ef4b41d50e9b1331d8bff67384ea14
Kulikov Nikita <nikitakulkov7@gmail.com>
2021-09-30 18:51:33 +0300

    add killing aliens

--- space_invaders.py
+++ space_invaders.py
@@ -68,6 +68,10 @@ class SpaceInvaders:
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
 
+        collisions = pygame.sprite.groupcollide(
+            self.bullets, self.aliens, True, True
+        )
+
     def _update_aliens(self):
         self._check_fleet_edges()
         self.aliens.update()
